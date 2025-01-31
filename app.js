4bd276369bccd3e8407fc833e6f29f98b81b9cb8
บุ๊ค 55 <siwapatwan@gmail.com>
2025-01-31 23:46:28 +0700

    edit page all

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+function message() {
+  alert('ภาษีของคุณคือ 0 บาท');
+}
 
+// ฟังก์ชันเปิด Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// ฟังก์ชันปิด Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+// ปิด Modal เมื่อคลิกนอกกล่องเนื้อหา
+window.onclick = function (event) {
+  let modal = document.getElementById("myModal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
